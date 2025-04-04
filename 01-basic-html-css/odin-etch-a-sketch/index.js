9e4414bce285f0ae3aa8ab1572da70a8e8622f51
Javier Arce <jasearce@espol.edu.ec>
2025-04-04 13:32:40 -0500

    completed etch-a-sketch problem

---
+++ 01-basic-html-css/odin-etch-a-sketch/index.js
@@ -0,0 +1,53 @@
+
+function validateInput(message) {
+    let input;
+    do {
+        input = prompt(message);
+        if (input === null || input === '') return null;
+        input = Number(input);
+    } while (isNaN(input) || input <= 0 || input > 100);
+    
+    return input;
+}
+
+function createGrid(containerEl, rows, columns){
+    const gridSize = rows * columns;
+    for (let i = 0; i < gridSize; i++) {
+        const block = document.createElement("div");
+        block.setAttribute("class", "square");
+        block.style.flexBasis = `calc(100%/${columns})`;
+        block.style.aspectRatio = "1/1";
+        containerEl.appendChild(block);
+    }
+}
+
+function applyMouseoverEffect() {
+    // Creating the persistent trail when hovering
+    const divBlocks = document.querySelectorAll(".square");
+    divBlocks.forEach((block) => {
+        block.addEventListener("mouseover", ()=>{
+            block.style.backgroundColor = "#1c1c1c";
+        }); 
+    }); 
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    console.log("DOM fully loaded and parsed");
+    const container = document.querySelector(".container");
+    createGrid(container, 16, 16);
+    applyMouseoverEffect();
+});
+
+const createGridBtn = document.querySelector("button");
+createGridBtn.addEventListener("click", ()=>{
+    const rows = validateInput("Enter number of rows for grid: ");
+    const columns = validateInput("Enter number of columns for grid: ");
+
+    // Delete and redraw the grid
+    const container = document.querySelector(".container");
+    container.innerHTML = "";
+
+    createGrid(container, rows, columns);
+    applyMouseoverEffect();
+
+});
