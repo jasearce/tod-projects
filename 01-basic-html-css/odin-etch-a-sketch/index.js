0003ea97daaddb9befaccf1e795e079d31ac70a0
Javier Arce <jasearce@espol.edu.ec>
2025-04-04 15:53:46 -0500

    add rgb colors when hovering the square divs

--- 01-basic-html-css/odin-etch-a-sketch/index.js
+++ 01-basic-html-css/odin-etch-a-sketch/index.js
@@ -26,14 +26,37 @@ function applyMouseoverEffect() {
     const divBlocks = document.querySelectorAll(".square");
     divBlocks.forEach((block) => {
         block.addEventListener("mouseover", ()=>{
-            block.style.backgroundColor = "#1c1c1c";
+            if (toogleBtn.checked) {
+                block.style.backgroundColor = "#1c1c1c";
+            } else {
+                block.style.backgroundColor = randomColor();
+            }
         }); 
     }); 
 }
 
+function randomColor() {
+    let randomRGBValues = [];
+    for (let i = 0; i < 3; i++) {
+        const randomColor = Math.floor(Math.random() * 255);
+        randomRGBValues.push(randomColor);
+    }
+    return `rgb(${randomRGBValues[0]}, ${randomRGBValues[1]}, ${randomRGBValues[2]})`;
+}
+
+let toogleBtn;
+let container;
+
 document.addEventListener("DOMContentLoaded", ()=>{
     console.log("DOM fully loaded and parsed");
-    const container = document.querySelector(".container");
+    toogleBtn = document.querySelector('input[type="checkbox"]');
+
+    toogleBtn.addEventListener('click', ()=>{
+        //toogleBtn.checked = !toogleBtn.checked;
+        console.log('Toogle to: ', toogleBtn.checked);
+    });
+
+    container = document.querySelector(".container");
     createGrid(container, 16, 16);
     applyMouseoverEffect();
 });
@@ -44,7 +67,7 @@ createGridBtn.addEventListener("click", ()=>{
     const columns = validateInput("Enter number of columns for grid: ");
 
     // Delete and redraw the grid
-    const container = document.querySelector(".container");
+    //const container = document.querySelector(".container");
     container.innerHTML = "";
 
     createGrid(container, rows, columns);
